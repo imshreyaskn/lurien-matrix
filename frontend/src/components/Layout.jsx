2f1c8e4e69c57f48f39e0ea3a8df6b2f7082c52e
Shreyas <oyedrizzle@gmail.com>
2026-07-09 22:16:32 +0530

    style: remove purple background gradient and upgrade tiles to true translucent glass

--- frontend/src/components/Layout.jsx
+++ frontend/src/components/Layout.jsx
@@ -3,12 +3,7 @@ import FloatingIsland from './FloatingIsland';
 
 export default function Layout() {
   return (
-    <div className="relative h-screen w-screen overflow-hidden bg-luma-000/30 backdrop-blur-[80px]">
-      {/* Noise Overlay */}
-      <div 
-        className="absolute inset-0 pointer-events-none opacity-[0.06] z-0" 
-        style={{ backgroundImage: 'url("data:image/svg+xml,%3Csvg viewBox=\'0 0 400 400\' xmlns=\'http://www.w3.org/2000/svg\'%3E%3Cfilter id=\'noiseFilter\'%3E%3CfeTurbulence type=\'fractalNoise\' baseFrequency=\'2.5\' numOctaves=\'3\' stitchTiles=\'stitch\'/%3E%3C/filter%3E%3Crect width=\'100%25\' height=\'100%25\' filter=\'url(%23noiseFilter)\'/%3E%3C/svg%3E")' }}
-      ></div>
+    <div className="relative h-screen w-screen overflow-hidden">
       {/* Structural Watermark */}
       <div className="absolute inset-0 overflow-hidden pointer-events-none z-0">
         <div className="structural-text absolute -left-40 top-40 -rotate-90 origin-left">
